a18dcfafa16c04221047c8170f5532a58725437b
Colin MacDonald <colin@goinstant.com>
2014-03-31 20:40:28 -0300

    refactor

--- deploy/tags.sh
+++ deploy/tags.sh
@@ -1,31 +1,13 @@
 git fetch --tags
 
-# Is Tag
-TAG_EXISTS=`git show-ref --tags --hash | grep -w $TRAVIS_COMMIT | wc -l`
-BRANCH_IS_TAG=`git tag --points-at $TRAVIS_COMMIT | grep -w $TRAVIS_BRANCH | wc -l`
+LATEST_TAG_HASH=`git rev-list --tags --max-count=1`
 
-# Latest
-LATEST_TAG_COMMIT=`git rev-list --tags --max-count=1`
-LATEST_TAG=`git describe --tags --exact-match $LATEST_TAG_COMMIT | grep -w $TRAVIS_BRANCH | wc -l`
+IS_TAG=`git tag --points-at $TRAVIS_COMMIT | grep -w $TRAVIS_BRANCH | wc -l`
+IS_LATEST=`git describe --tags --exact-match $LATEST_TAG_HASH | grep -w $TRAVIS_BRANCH | wc -l`
 
-IS_LATEST=false
-IS_TAG=false
-
-if [ $TAG_EXISTS != 0 ] && [ $BRANCH_IS_TAG != 0 ]; then
-  IS_TAG=true
-fi
-
-if [ $LATEST_TAG != 0 ]; then
-  IS_LATEST=true
-fi
-
+echo "Latest tag hash: $LATEST_TAG_HASH"
 echo "Is tag: $IS_TAG"
 echo "Is latest: $IS_LATEST"
 
-echo $TAG_EXISTS
-echo $LATEST_TAG
-echo $BRANCH_IS_TAG
-echo $LATEST_TAG_COMMIT
-
-export IS_LATEST
 export IS_TAG
+export IS_LATEST
