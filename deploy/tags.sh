1bb646e5d7647f1502f5216f3b3178ddf381ede0
Colin MacDonald <colin@goinstant.com>
2014-03-31 17:15:03 -0300

    ok

--- deploy/tags.sh
+++ deploy/tags.sh
@@ -1,32 +1,28 @@
-#TRAVIS_COMMIT=9bcfe87f6805b4783439205e2d15e98f295d9e21
-#TRAVIS_BRANCH=v3.3.15
+#TRAVIS_COMMIT=5d70fa866b5b7dc1c558e57ae68ba9111b05b809
+#TRAVIS_BRANCH=v3.3.22
 
-TAG_LATEST=`git rev-list --tags --max-count=1`
-TAGS_HASH=`git show-ref --tags --hash`
-TAGS_HAS_HASH=`grep -q -w $TRAVIS_COMMIT <<<$TAGS_HASH`
-POINTS_TO_TAG=`git tag --points-at $TRAVIS_COMMIT`
+#TAG_LATEST=`git rev-list --tags --max-count=1`
+TAG_EXISTS=`git show-ref --tags --hash | grep -w $TRAVIS_COMMIT | wc -l`
+LATEST_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT`
 
 IS_LATEST=false
 IS_TAG=false
 
-# Check if commit hash exists in list of tag hashes
-# and the tag points to the TRAVIS_COMMIT
-if [ $TRAVIS_BRANCH == "$POINTS_TO_TAG" ] && $CONTAINS_HASH; then
+if [ $TAG_EXISTS != 0 ]; then
   IS_TAG=true
 fi
 
-# Check if latest tag hash matches the commit hash
-if [ $TAG_LATEST == "$TRAVIS_COMMIT" ]; then
+if [ $LATEST_TAG == $TRAVIS_BRANCH ]; then
   IS_LATEST=true
 fi
 
-echo $IS_LATEST
-echo $IS_TAG
+echo "Is tag: $IS_TAG"
+echo "Is latest: $IS_LATEST"
+
+echo $TAG_EXISTS
+echo $LATEST_TAG
 
 export IS_LATEST
 export IS_TAG
-export TRUE=true
-export FALSE=false
-export TEST=work
 
-printenv
+#printenv
