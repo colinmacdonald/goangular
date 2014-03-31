e41845d5200367d2db17f3b3a341d396106d031c
Colin MacDonald <colin@goinstant.com>
2014-03-31 17:51:01 -0300

    new

--- deploy/tags.sh
+++ deploy/tags.sh
@@ -1,6 +1,3 @@
-#TRAVIS_COMMIT=5d70fa866b5b7dc1c558e57ae68ba9111b05b809
-#TRAVIS_BRANCH=v3.3.23
-
 TAG_EXISTS=`git show-ref --tags --hash | grep -w $TRAVIS_COMMIT | wc -l`
 BRANCH_IS_TAG=`git tag --points-at $TRAVIS_COMMIT | grep -w $TRAVIS_BRANCH | wc -l`
 LATEST_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT | grep -w $TRAVIS_BRANCH | wc -l`
