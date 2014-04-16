6cba3ab3fd870e0468f8a33f79731529980ac592
Colin MacDonald <colin@goinstant.com>
2014-04-16 13:53:30 -0300

    latest

--- deploy/hipchat-notify.js
+++ deploy/hipchat-notify.js
@@ -8,13 +8,16 @@ var _ = require('lodash');
 var HIPCHAT_TOKEN = process.env.HIPCHAT_TOKEN;
 var BRANCH = process.env.TRAVIS_BRANCH;
 var ROOMS = process.env.ROOMS.split(/,\s?/);
+var IS_LATEST = parseInt(process.env.IS_LATEST);
+var LATEST = IS_LATEST !== 0 ? '/latest' : '';
 
 var hipchatter = new Hipchatter(HIPCHAT_TOKEN, 'https://api.hipchat.com/v2/');
 
 var message = 'Update of <a href="http://github.com/goinstant/goangular/' +
-              'releases/tag/' + BRANCH +'">GoAngular:' + BRANCH +'</a> to ' +
-              '<a href="https://cdn.goinstant.net/integrations/goangular/' +
-              BRANCH + '/goangular.js">Production</a> was successful!';
+              'releases/tag/' + BRANCH +'">GoAngular:' + BRANCH + LATEST +
+              '</a> to <a href="https://cdn.goinstant.net/integrations/' +
+              'goangular/' + BRANCH + '/goangular.js">Production</a> was' +
+              'successful!';
 
 var opts = {
   message: message,
